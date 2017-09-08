7d5a858846a42aafc68f75bc8c38c85085491383
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-09-08 11:53:21 +0200

    Using MIDL client

--- midl/main.c
+++ midl/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #include "Setup.Configuration_h.h"
+#include "Setup.Configuration_i.c"
 
 #ifndef UNREFERENCED_PARAMETER
 #define UNREFERENCED_PARAMETER(x) (x)
@@ -11,14 +12,11 @@ static void print_error(HRESULT hr)
 {
 	char hr_message[1000];
 	DWORD hr_message_len = FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, NULL, hr, 0, hr_message, sizeof(hr_message) / sizeof(hr_message[0]), NULL);
-	fprintf(stderr, "%*.*s", (int)hr_message_len, sizeof(hr_message) / sizeof(hr_message[0]), hr_message);
+	fprintf(stderr, "%*.*s", (int)hr_message_len, (int)(sizeof(hr_message) / sizeof(hr_message[0])), hr_message);
 }
 
 int main(int argc, char* argv[])
 {
-	IID IID_ISetupConfiguration_local;
-	IID IID_ISetupConfiguration2_local;
-	CLSID CLSID_SetupConfiguration;
 	HRESULT hr;
 	ISetupConfiguration* setupConfiguration;
 	ISetupConfiguration2* setupConfiguration2;
@@ -27,15 +25,12 @@ int main(int argc, char* argv[])
 	UNREFERENCED_PARAMETER(argv);
 
 #define HRESULT_CHECKED(x) hr = x; if FAILED(hr) goto error
-	HRESULT_CHECKED(CLSIDFromString(OLESTR("{177F0C4A-1CD3-4DE7-A32C-71DBBB9FA36D}"), &CLSID_SetupConfiguration));
-	HRESULT_CHECKED(IIDFromString(OLESTR("{42843719-DB4C-46C2-8E7C-64F1816EFD5B}"), &IID_ISetupConfiguration_local));
-	HRESULT_CHECKED(IIDFromString(OLESTR("{26AAB78C-4A60-49D6-AF3B-3C35BC93365D}"), &IID_ISetupConfiguration2_local));
 	HRESULT_CHECKED(CoInitialize(NULL));
 
 #define printf_instance(type, instance) printf("%s: %s (0x%p)\n", #instance, #type, instance);
-#define printf_field(pInstance, vtblType, member) printf("\t[%02d] 0x%p: %s\n", offsetof(vtblType, member), pInstance->lpVtbl->member, #member)
+#define printf_field(pInstance, vtblType, member) printf("\t[%02d] 0x%p: %s\n", (int)(offsetof(vtblType, member)), pInstance->lpVtbl->member, #member)
 
-	HRESULT_CHECKED(CoCreateInstance(&CLSID_SetupConfiguration, NULL, CLSCTX_INPROC, &IID_ISetupConfiguration_local, &setupConfiguration));
+	HRESULT_CHECKED(CoCreateInstance(&CLSID_SetupConfiguration, NULL, CLSCTX_INPROC, &IID_ISetupConfiguration, &setupConfiguration));
 	printf_instance(ISetupConfiguration*, setupConfiguration);
 	printf_field(setupConfiguration, ISetupConfigurationVtbl, QueryInterface);
 	printf_field(setupConfiguration, ISetupConfigurationVtbl, AddRef);
@@ -44,7 +39,7 @@ int main(int argc, char* argv[])
 	printf_field(setupConfiguration, ISetupConfigurationVtbl, GetInstanceForCurrentProcess);
 	printf_field(setupConfiguration, ISetupConfigurationVtbl, GetInstanceForPath);
 
-	HRESULT_CHECKED(setupConfiguration->lpVtbl->QueryInterface(setupConfiguration, &IID_ISetupConfiguration2_local, &setupConfiguration2));
+	HRESULT_CHECKED(setupConfiguration->lpVtbl->QueryInterface(setupConfiguration, &IID_ISetupConfiguration2, &setupConfiguration2));
 	printf_instance(ISetupConfiguration2*, setupConfiguration);
 	printf_field(setupConfiguration2, ISetupConfiguration2Vtbl, QueryInterface);
 	printf_field(setupConfiguration2, ISetupConfiguration2Vtbl, AddRef);
